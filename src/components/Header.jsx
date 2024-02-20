0b45a56769b8e692d160340e973c890447101867
Maicol Salazar <stivsal12@gmail.com>
2024-02-19 19:25:50 -0500

    correccion 1.1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header>
          <div className='header_img'>
-        <img src='../public/img/log1.1.png' width='100%'></img>
+        <img src='/img/log1.1.png' width='100%'></img>
       </div>
     </header>
   )
